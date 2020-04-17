4caee9892cfd2bd42870118f574df64dcf02d4e4
Kara3 <elena.lilova@gmx.de>
2020-04-17 13:17:26 +0200

    master new pull

---
+++ src/main/java/edu/hm/hafner/analysis/IssueDifferenceBuilder.java
@@ -0,0 +1,62 @@
+package edu.hm.hafner.analysis;
+
+import java.util.Objects;
+
+/**
+ * Creates new {@link IssueDifference} using the builder pattern.
+ *
+ * @author Elena Lilova
+ */
+
+public class IssueDifferenceBuilder {
+
+    private Report currentIssues;
+    private Report referenceIssues;
+    private String referenceID;
+
+    /**
+     * Sets the the currentIssues
+     *
+     * @return this
+     */
+    public IssueDifferenceBuilder setCurrentIssues(final Report currentIssues) {
+        this.currentIssues = currentIssues;
+        return this;
+    }
+
+    /**
+     * Sets the the referenceIssues
+     *
+     * @return this
+     */
+    public IssueDifferenceBuilder setReferenceIssues(final Report referenceIssues) {
+        this.referenceIssues = referenceIssues.copy();
+        return this;
+    }
+
+    /**
+     * Sets the the referenceId
+     *
+     * @return this
+     */
+    public IssueDifferenceBuilder setReferenceID(final String referenceId) {
+        this.referenceID = referenceId;
+        return this;
+    }
+
+
+    /**
+     * Creates a new {@link IssueDifference} based on the specified properties.
+     *
+     * @return the created IssueDifference
+     */
+    public IssueDifference build() {
+        Objects.requireNonNull(currentIssues);
+        Objects.requireNonNull(referenceID);
+        Objects.requireNonNull(referenceIssues);
+
+        return new IssueDifference(currentIssues, referenceID, referenceIssues);
+
+    }
+
+}
